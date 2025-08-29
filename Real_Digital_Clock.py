3fefdd8987868fca0c130f9a07fa8b079b027414
Noor-abid <abidnoor38544@gmail.com>
2025-08-30 00:01:02 +0500

    final

--- Real_Digital_Clock.py
+++ Real_Digital_Clock.py
@@ -4,7 +4,7 @@ from tkinter import Label, Tk
 # Window config for Clock
 window =Tk()
 window.title("Digital Clock")
-window.geometry("200 x 80")
+window.geometry("200x80")
 window.configure(bg="black")
 window.resizable(False, False)
 
@@ -12,7 +12,7 @@ window.resizable(False, False)
 clock_label=Label(window, bg="black", fg="white", font=("Times", 30, 'bold'), relief='flat')
 clock_label.place(x=20, y=20)
 def updating_label():
-    current_time=strftime('%H: %M: %S')
+    current_time=strftime('%H:%M:%S')
     clock_label.configure(text=current_time)
     clock_label.after(80, updating_label)
 
