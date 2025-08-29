9d0d7b432570afa6c74dabb229b75b04950b07cd
Noor-abid <abidnoor38544@gmail.com>
2025-08-29 23:49:09 +0500

    initial commit

--- Real_Digital_Clock.py
+++ Real_Digital_Clock.py
@@ -11,3 +11,10 @@ window.resizable(False, False)
 #label config
 clock_label=Label(window, bg="black", fg="white", font=("Times", 30, 'bold'), relief='flat')
 clock_label.place(x=20, y=20)
+def updating_label():
+    current_time=strftime('%H: %M: %S')
+    clock_label.configure(text=current_time)
+    clock_label.after(80, updating_label)
+
+updating_label()
+window.mainloop()    
