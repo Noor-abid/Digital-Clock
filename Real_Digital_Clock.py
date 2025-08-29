4e2ca1be555fd8f701383cd4335fc85b9bc08b08
Noor-abid <abidnoor38544@gmail.com>
2025-08-29 23:39:59 +0500

    initial commit

--- Real_Digital_Clock.py
+++ Real_Digital_Clock.py
@@ -2,3 +2,12 @@ from time import strftime
 from tkinter import Label, Tk
 
 # Window config for Clock
+window =Tk()
+window.title("Digital Clock")
+window.geometry("200 x 80")
+window.configure(bg="black")
+window.resizable(False, False)
+
+#label config
+clock_label=Label(window, bg="black", fg="white", font=("Times", 30, 'bold'), relief='flat')
+clock_label.place(x=20, y=20)
